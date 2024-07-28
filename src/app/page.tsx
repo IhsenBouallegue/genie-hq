9a395f40e28cd9c52cba0c234772d6df454c6764
Ihsen Bouallegue <bouallegueihsen@gmail.com>
2024-07-28 14:28:42 +0200

    refactor: Remove BackgroundBeams component from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Faq from "@/components/faq";
 import { Features } from "@/components/features";
 import Hero from "@/components/hero";
-import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function Home() {
   return (
