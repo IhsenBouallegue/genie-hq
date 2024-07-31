a69da9d2c28747704e1428f6de15b511adb391b3
IhsenBouallegue <IhsenBouallegue@gmail.com>
2024-07-31 23:59:04 +0200

    feat: Add stepper component and setup configurator tool

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import AboutUs from "@/components/abouts-us";
 import Faq from "@/components/faq";
 import { Features } from "@/components/features";
 import Hero from "@/components/hero";
+import SetupConfiguratorTool from "@/components/setup-configurator-tool";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col gap-24 items-center ">
       <Hero />
+      <SetupConfiguratorTool />
       <Features />
       <Faq />
       <AboutUs />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,16 +76,26 @@ const config = {
           "0%": { backgroundPosition: "top center" },
           "100%": { backgroundPosition: "bottom center" },
         },
-		"shine-pulse": {
-			"0%": { backgroundPosition: "0% 0%" },
-			"50%": { backgroundPosition: "100% 100%" },
-			to: { backgroundPosition: "0% 0%" },
-		},
+        "shine-pulse": {
+          "0%": { backgroundPosition: "0% 0%" },
+          "50%": { backgroundPosition: "100% 100%" },
+          to: { backgroundPosition: "0% 0%" },
+        },
+        "collapsible-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-collapsible-content-height)" },
+        },
+        "collapsible-up": {
+          from: { height: "var(--radix-collapsible-content-height)" },
+          to: { height: "0" },
+        },
       },
 
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "collapsible-down": "collapsible-down 0.2s ease-out",
+        "collapsible-up": "collapsible-up 0.2s ease-out",
         backgroundPositionSpin:
           "background-position-spin 3000ms infinite alternate",
       },
