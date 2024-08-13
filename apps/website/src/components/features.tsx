ce1df58540e05bd76e720b768bde293032c5f76b
Ihsen Bouallegue <bouallegueihsen@gmail.com>
2024-08-13 23:48:10 +0200

    chore: Update features component with new security and admin rights sections

--- apps/website/src/components/features.tsx
+++ apps/website/src/components/features.tsx
@@ -1,9 +1,13 @@
 import { SiApple, SiLinux, SiWindows11 } from "@icons-pack/react-simple-icons";
 import {
   CheckSquareIcon,
+  KeyIcon,
   LayoutIcon,
+  LockKeyhole,
   MonitorSmartphoneIcon,
   Settings2Icon,
+  ShieldCheckIcon,
+  TerminalIcon,
   UserRound,
 } from "lucide-react";
 import { Circle } from "./circle";
@@ -34,11 +38,45 @@ export function Features() {
 
 const items = [
   {
+    title: "Security and Transparency",
+    description:
+      "We source all apps from official repositories, using winget, brew and scoop for transparent, secure installations.",
+    header: (
+      <div className="h-full flex items-center justify-center gap-8 rounded-lg border p-6 md:shadow-xl">
+        <Circle>
+          <ShieldCheckIcon strokeWidth={2.5} className="text-green-600" />
+        </Circle>
+        <Circle>
+          <LockKeyhole strokeWidth={2.5} className="text-black" />
+        </Circle>
+      </div>
+    ),
+    className: "md:col-span-3",
+    icon: <ShieldCheckIcon className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "No Admin Rights",
+    description:
+      "Install applications without admin rights using the scoop package manager, allowing for a smoother setup process.",
+    header: (
+      <div className="h-full flex items-center justify-center gap-8 rounded-lg border p-6 md:shadow-xl">
+        <Circle>
+          <KeyIcon strokeWidth={2.5} className="text-yellow-500" />
+        </Circle>
+        <Circle>
+          <TerminalIcon strokeWidth={2.5} className="text-black" />
+        </Circle>
+      </div>
+    ),
+    className: "md:col-span-3",
+    icon: <KeyIcon className="h-4 w-4 text-neutral-500" />,
+  },
+  {
     title: "Effortless Setup",
     description:
       "Experience the simplicity of setting up your device with GenieHQ. Automatically install essential programs and applications tailored to your needs.",
     header: <FeatureAnimatedProgram />,
-    className: "md:col-span-2",
+    className: "md:col-span-4",
     icon: <CheckSquareIcon className="h-4 w-4 text-neutral-500" />,
   },
   {
@@ -58,7 +96,7 @@ const items = [
         </Circle>
       </div>
     ),
-    className: "md:col-span-1",
+    className: "md:col-span-2",
     icon: <MonitorSmartphoneIcon className="h-4 w-4 text-neutral-500" />,
   },
   {
@@ -66,7 +104,7 @@ const items = [
     description:
       "Sync your GenieHQ account across multiple devices for a consistent and up-to-date setup.",
     header: <FeatureAccountSync />,
-    className: "md:col-span-1",
+    className: "md:col-span-2",
     icon: <LayoutIcon className="h-4 w-4 text-neutral-500" />,
   },
   {
@@ -89,7 +127,7 @@ const items = [
         </Circle>
       </div>
     ),
-    className: "md:col-span-2",
+    className: "md:col-span-4",
     icon: <Settings2Icon className="h-4 w-4 text-neutral-500" />,
   },
 ];
