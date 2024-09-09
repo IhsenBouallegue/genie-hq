2df88d71eb18f678ff299389f6e7b3a48e28ebf1
Enrico <77448275+bic-marz@users.noreply.github.com>
2024-09-09 16:37:53 +0000

    refactor: Update drizzle.config.ts and populateDB.ts
    Remove dotenv import and configuration from drizzle.config.ts.
    Add support for fetching and saving Homebrew formula and cask apps to the database in populateDB.ts.

--- apps/desktop-app/src/db/populateDB.ts
+++ apps/desktop-app/src/db/populateDB.ts
@@ -47,6 +47,8 @@ const checkGithubApiRateLimit = async () => {
 const SCOOPSEARCH_URL = process.env.SCOOPSEARCH_URL;
 const SCOOPSEARCH_API_KEY = process.env.SCOOPSEARCH_API_KEY;
 const WINGET_API_URL = process.env.WINGET_API_URL;
+const HOMEBREW_FORMULA_API_URL = process.env.HOMEBREW_FORMULA_API_URL;
+const HOMEBREW_CASK_API_URL = process.env.HOMEBREW_CASK_API_URL;
 
 // Function to fetch official Scoop apps
 async function fetchOfficialScoopApps() {
@@ -294,9 +296,177 @@ async function saveWingetAppsToDatabase(apps: any[]) {
   }
 }
 
-const fetchHomebrewApps = async (): Promise<InsertApplication[]> => {
-  throw new Error("Not implemented");
-};
+// Function to fetch Homebrew formula apps
+async function fetchHomebrewFormulaApps(): Promise<any[]> {
+  const response = await fetch(HOMEBREW_FORMULA_API_URL || "", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    console.error(
+      `Failed to fetch Homebrew formula apps: ${response.statusText}`,
+    );
+    return [];
+  }
+
+  const data = await response.json();
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  const apps = data.map((formula: any) => {
+    const isDeprecated = formula.deprecated;
+    const isOutdated = formula.outdated;
+    const deprecationDate = formula.deprecation_date
+      ? new Date(formula.deprecation_date).toLocaleDateString()
+      : null;
+    const disableDate = formula.disable_date
+      ? new Date(formula.disable_date).toLocaleDateString()
+      : null;
+
+    let notes = `License: ${formula.license || "Unknown license"}.`;
+    if (formula.caveats) notes += ` ${formula.caveats}`;
+    if (isDeprecated)
+      notes += ` This app is deprecated${deprecationDate ? ` since ${deprecationDate}` : ""}.`;
+    if (isOutdated) notes += " This app is outdated.";
+    if (disableDate) notes += ` Disabled since ${disableDate}.`;
+
+    let iconUrl: string;
+
+    if (formula.homepage === "" || formula.homepage === null) {
+      iconUrl = "https://formulae.brew.sh/assets/img/homebrew.svg";
+    } else {
+      try {
+        const url = new URL(formula.homepage);
+        iconUrl = `${url.protocol}//${url.hostname}/favicon.ico`;
+      } catch (error) {
+        console.error(`Invalid URL: ${formula.homepage}`, error);
+        iconUrl = "https://formulae.brew.sh/assets/img/homebrew.svg";
+      }
+    }
+
+    return {
+      name: formula.name,
+      description: formula.desc || "",
+      homepage: formula.homepage || "",
+      version: formula.versions.stable || "",
+      installCommand: `brew install ${formula.name}`,
+      iconUrl: iconUrl,
+      notes: notes.trim(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+  });
+
+  console.log(`Fetched ${apps.length} Homebrew formula apps.`);
+  return apps;
+}
+
+// Function to fetch Homebrew cask apps
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+async function fetchHomebrewCaskApps(): Promise<any[]> {
+  const response = await fetch(HOMEBREW_CASK_API_URL || "", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    console.error(`Failed to fetch Homebrew cask apps: ${response.statusText}`);
+    return [];
+  }
+
+  const data = await response.json();
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  const apps = data.map((cask: any) => {
+    const isDeprecated = cask.deprecated;
+    const isOutdated = cask.outdated;
+    const deprecationDate = cask.deprecation_date
+      ? new Date(cask.deprecation_date).toLocaleDateString()
+      : null;
+    const disableDate = cask.disable_date
+      ? new Date(cask.disable_date).toLocaleDateString()
+      : null;
+
+    let notes = `License: ${cask.license || "Unknown license"}.`;
+    if (cask.caveats) notes += ` ${cask.caveats}`;
+    if (isDeprecated)
+      notes += ` This app is deprecated${deprecationDate ? ` since ${deprecationDate}` : ""}.`;
+    if (isOutdated) notes += " This app is outdated.";
+    if (disableDate) notes += ` Disabled since ${disableDate}.`;
+
+    let iconUrl: string;
+
+    if (cask.homepage === "" || cask.homepage === null) {
+      iconUrl = "https://formulae.brew.sh/assets/img/homebrew.svg";
+    } else {
+      try {
+        const url = new URL(cask.homepage);
+        iconUrl = `${url.protocol}//${url.hostname}/favicon.ico`;
+      } catch (error) {
+        console.error(`Invalid URL: ${cask.homepage}`, error);
+        iconUrl = "https://formulae.brew.sh/assets/img/homebrew.svg";
+      }
+    }
+
+    return {
+      name: cask.token,
+      description: cask.desc || "",
+      homepage: cask.homepage || "",
+      version: cask.version || "",
+      installCommand: `brew install ${cask.token}`,
+      iconUrl: iconUrl,
+      notes: notes.trim(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+  });
+
+  console.log(`Fetched ${apps.length} Homebrew cask apps.`);
+  return apps;
+}
+
+// Function to save Homebrew apps to the database
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+async function saveHomebrewAppsToDatabase(apps: any[]) {
+  let count = 0;
+  for (const app of apps) {
+    const appData: InsertApplication = {
+      name: app.name,
+      description: app.description || "",
+      homepage: app.homepage || "",
+      version: app.version || "",
+      iconUrl: app.iconUrl || "",
+      category: "Misc",
+      tags: [],
+      images: [],
+      notes: app.notes || "",
+      createdAt: app.createdAt || new Date(),
+      updatedAt: app.updatedAt || new Date(),
+    };
+
+    const [insertedApp] = await db
+      .insert(applicationsTable)
+      .values(appData)
+      .returning();
+
+    if (!insertedApp) {
+      throw new Error("Failed to insert Homebrew application");
+    }
+
+    await db.insert(installationMethodsTable).values({
+      applicationId: insertedApp.id,
+      osId: 2,
+      packageManagerId: 3,
+      command: app.installCommand,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    if (count++ % 100 === 0) {
+      console.log(`Saved ${count} Homebrew apps to the database so far...`);
+    }
+  }
+}
 
 /**
  * Insert the package managers into the database.
@@ -421,8 +591,14 @@ async function main() {
     // console.log(`Fetched ${wingetApps.length} Winget apps.`);
     // await saveWingetAppsToDatabase(wingetApps);
 
-    // step 4: fetch and save homebrew apps to the database
-    const homebrewApps = await fetchHomebrewApps(); // TODO: Implement fetchHomebrewApps
+    // step 4a: fetch and save homebrew formula apps to the database
+    // const homebrewFormulaApps = await fetchHomebrewFormulaApps();
+    // await saveHomebrewAppsToDatabase(homebrewFormulaApps);
+
+    // step 4b: fetch and save homebrew cask apps to the database
+    // const homebrewCaskApps = await fetchHomebrewCaskApps();
+    // await saveHomebrewAppsToDatabase(homebrewCaskApps);
+    console.log("All apps have been saved to the database.");
   } catch (error) {
     console.error("Error saving apps to the database:", error);
   }
