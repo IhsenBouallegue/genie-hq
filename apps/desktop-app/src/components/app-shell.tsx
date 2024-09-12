93c705fe134c021377b60c14d3af697f09a11ad8
IhsenBouallegue <IhsenBouallegue@gmail.com>
2024-09-12 11:18:41 +0200

    add header

--- apps/desktop-app/src/components/app-shell.tsx
+++ apps/desktop-app/src/components/app-shell.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Header from "./header";
 import Sidebar from "./sidebar";
 
 export function AppShell({
@@ -10,10 +11,13 @@ export function AppShell({
       <div className="hidden border-r bg-muted/40 md:block">
         <Sidebar />
       </div>
-      <div className="flex flex-col h-[100vh] overflow-y-scroll">
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          {children}
-        </main>
+      <div>
+        <Header />
+        <div className="h-[calc(100vh-3.5rem)] lg:h-[calc(100vh-60px)] overflow-y-scroll">
+          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+            {children}
+          </main>
+        </div>
       </div>
     </div>
   );
---
+++ apps/desktop-app/src/components/header.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@geniehq/ui/components/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@geniehq/ui/components/dropdown-menu";
+import { Input } from "@geniehq/ui/components/input";
+import { CircleUser, Search } from "lucide-react";
+
+export default function Header() {
+  return (
+    <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+      <div className="w-full flex-1">
+        <form>
+          <div className="relative">
+            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Search apps..."
+              className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+              disabled
+            />
+          </div>
+        </form>
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="secondary" size="icon" className="rounded-full">
+            <CircleUser className="h-5 w-5" />
+            <span className="sr-only">Toggle user menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </header>
+  );
+}
